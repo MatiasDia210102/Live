a4c96c580351f4ed474ede8bc9b39c4ccbd1b530
MatiasDia210102 <matiu210102dewilde@gmail.com>
2025-11-13 09:54:42 -0300

    modifico url

--- src/componentes/AuthContext.jsx
+++ src/componentes/AuthContext.jsx
@@ -4,9 +4,9 @@ export const ROLES = {
     ADMIN: 'administrador',
     ESPECTADOR: 'espectador',
 };
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001';
+const API_URL = `${BASE_URL}/api`;
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-const API_URL = `${BASE_URL || 'http://localhost:3001'}/api`;
 const ContextoAuth = createContext();
 export const useAuth = () => useContext(ContextoAuth);
 
